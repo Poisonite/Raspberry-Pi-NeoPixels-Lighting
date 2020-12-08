8444110f85e4ca445b5d04625f46c4aed2e17b06
Zac <zacace@ingoglia.com>
2020-12-07 18:48:43 -0700

    Format Update
    Got the linter and formatter working so now both files follow a standard convention

--- treeLighting_dynamic.py
+++ treeLighting_dynamic.py
@@ -3,45 +3,45 @@ import neopixel
 from time import sleep
 
 # Welcome Message
-print ("Hello, Light Master!")
+print("Hello, Light Master!")
 
 # Set Config Params
-print ("Enter number of pixels to modify:")
+print("Enter number of pixels to modify:")
 pixelCount = input()
 pixelCount = int(pixelCount)
 
-print ("Enter brightness of pixels (float between 0-1):")
+print("Enter brightness of pixels (float between 0-1):")
 brightness = input()
 brightness = float(brightness)
 
-print ("Enter RGB data 1 by 1(int between 0-255).")
-print ("Red: ")
+print("Enter RGB data 1 by 1(int between 0-255).")
+print("Red: ")
 red = input()
 red = int(red)
-print ("Green: ")
+print("Green: ")
 green = input()
 green = int(green)
-print ("Blue: ")
+print("Blue: ")
 blue = input()
 blue = int(blue)
 
 # Output user's config info
-print ("Beginning test light operation with perams: ")
-print ('Pixels: {pixelCount}')
-print ('Brightness: {brightness}')
-print ('Color: ({red}, {green}, {blue})')
+print("Beginning test light operation with perams: ")
+print('Pixels: {pixelCount}')
+print('Brightness: {brightness}')
+print('Color: ({red}, {green}, {blue})')
 
 # Assign the pixels to be modified and give the new brightness
-pixels = neopixel.NeoPixel(board.D18, pixelCount, brightness =brightness)
+pixels = neopixel.NeoPixel(board.D18, pixelCount, brightness=brightness)
 
 # Clear any Existing color data
 pixels.fill((0, 0, 0))
-print ("Cleared existing pixel info.")
-sleep (2)
+print("Cleared existing pixel info.")
+sleep(2)
 # Assign the new colors
 pixels.fill((red, green, blue))
 pixels.show()
-print ('Updated pixel color info to: ({red}, {green}, {blue}).')
+print('Updated pixel color info to: ({red}, {green}, {blue}).')
 
-#Notify user of script completion
-print ("Finished sending pixel instructions.")
+# Notify user of script completion
+print("Finished sending pixel instructions.")
--- treeLighting_static.py
+++ treeLighting_static.py
@@ -3,25 +3,25 @@ import neopixel
 from time import sleep
 
 # Welcome Message
-print ("Hello, Light Master!")
+print("Hello, Light Master!")
 
 # Output user's config info
-print ("Beginning test light operation with perams: ")
-print ('Pixels: 10')
-print ('Brightness: 1')
-print ('Color: (255, 0, 0)')
+print("Beginning test light operation with perams: ")
+print('Pixels: 10')
+print('Brightness: 1')
+print('Color: (255, 0, 0)')
 
 # Assign the pixels to be modified and give the new brightness
-pixels = neopixel.NeoPixel(board.D18, 10, brightness =1)
+pixels = neopixel.NeoPixel(board.D18, 10, brightness=1)
 
 # Clear any Existing color data
 pixels.fill((0, 0, 0))
-print ("Cleared existing pixel info.")
-sleep (2)
+print("Cleared existing pixel info.")
+sleep(2)
 # Assign the new colors
 pixels.fill((255, 0, 0))
 pixels.show()
-print ('Updated pixel color info to: (255, 0, 0).')
+print('Updated pixel color info to: (255, 0, 0).')
 
-#Notify user of script completion
-print ("Finished sending pixel instructions.")
+# Notify user of script completion
+print("Finished sending pixel instructions.")
