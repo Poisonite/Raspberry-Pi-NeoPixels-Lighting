5bc1c733f43685abff5b47523fad48e874c66383
Zac <poisonite@gmail.com>
2021-12-20 02:33:41 -0700

    Improved and refactored classic lights
    Defined all colors and other important settings as vars at the top of the file
    Changed twinkle settings to use a range instead of static values (to increase randomness appearance)
    Added additional informational print statements
    Added comments and cleaned up code structure
    Moved to a structured class system for twinkle pixel storage
    moved to for loop and array system for pulling and setting twinkle pixels instead of static vars
    Used need class system to structure pixel data being sent to variables

--- classicLights.py
+++ classicLights.py
@@ -1,8 +1,25 @@
+# Python core imports
 import random
 from time import sleep
 
+# Import GPIO ref and pixel instructions
 import board
 import neopixel
+
+# Base Settings
+pixelCount = 2000
+brightness = 0.4
+
+# Color Settings
+baseColor1 = (190, 60, 0)
+baseColor2 = (255, 189, 109)
+twinkleColor = (255, 250, 236)
+
+# Twinkle Settings
+twinkleCountMin = 10
+twinkleCountMax = 50
+twinkleTimeMin = 0.01
+twinkleTimeMax = 0.3
 
 # Welcome Message
 print("Hello, Light Master!\n")
@@ -10,55 +27,57 @@ print("Beginning the classic light show!")
 
 # Assign the pixels to be modified and give the new brightness
 pixels = neopixel.NeoPixel(
-    board.D18, 2000, brightness=0.4, auto_write=False, pixel_order=neopixel.RGB)
+    board.D18, pixelCount, brightness=brightness, auto_write=False, pixel_order=neopixel.RGB)
 
 # Clear any Existing color data
 pixels.fill((0, 0, 0))
 pixels.show()
-print("Cleared existing pixel info.")
+print("Cleared existing pixel info")
 sleep(0.5)
 
 # Set Base color
-for i in range(2000):
+print("Setting base color per pixel")
+for i in range(pixelCount):
     if (i % 2) == 0:
-        pixels[i] = (190, 60, 0)
+        pixels[i] = baseColor1
     else:
-        pixels[i] = (255, 189, 109)
+        pixels[i] = baseColor2
 pixels.show()
 
+# Define a structued class for the pixel data
+class PixelSettings:
+  def __init__(self, num, orgColor):
+    self.num = num
+    self.orgColor = orgColor
+
 # Begin twinkle
+print("Start twinkle pattern")
 while True:
+    # Store the pixels which will be changed
+    pixelSettingArr = []
+
     # Get pixel list
-    i = random.randint(0, 1999)
-    j = random.randint(0, 1999)
-    k = random.randint(0, 1999)
-    l = random.randint(0, 1999)
-    m = random.randint(0, 1999)
-    n = random.randint(0, 1999)
+    for i in range(random.randint(twinkleCountMin, twinkleCountMax)):
+        # Define important info for the pixel to be changed
+        pixelNum = random.randint(0, pixelCount-1)
+        pixelColor = pixels[pixelNum]
 
-    # Collect existing pixel info
-    iColor = pixels[i]
-    jColor = pixels[j]
-    kColor = pixels[k]
-    lColor = pixels[l]
-    mColor = pixels[m]
-    nColor = pixels[n]
+        # Set white color on twinkle pixels
+        pixels[pixelNum] = twinkleColor
 
-    # Set white color on twinkle pixels
-    pixels[i] = (255, 250, 236)
-    pixels[j] = (255, 250, 236)
-    pixels[k] = (255, 250, 236)
-    pixels[l] = (255, 250, 236)
-    pixels[m] = (255, 250, 236)
-    pixels[n] = (255, 250, 236)
+        # Structure the collected data
+        pixelSetting = PixelSettings(pixelNum, pixelColor)
+
+        # Add structured settings to storage arr
+        pixelSettingArr.append(pixelSetting)
+
+    # Output the finished set of changes and wait
     pixels.show()
-    sleep(0.15)
+    sleep(random.uniform(twinkleTimeMin, twinkleTimeMax))
 
-    # Set twinkle pixels back to normal
-    pixels[i] = iColor
-    pixels[k] = jColor
-    pixels[l] = kColor
-    pixels[m] = lColor
-    pixels[n] = mColor
-    pixels[j] = nColor
+    # Set twinkle pixels back to org
+    for pixelSetting in pixelSettingArr:
+        pixels[pixelSetting.num] = pixelSetting.orgColor
+
+    # Output the reset pixels
     pixels.show()
